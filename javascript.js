bee76390bd34840da0c3561ad1ef05126d0e0701
Sreenath1199 <sreenathsreenivasan11@gmail.com>
2021-07-17 17:56:40 +0530

    Coin toss by Sreenath

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// function getRandomItem(arr) {
+//   // get random index value
+//   const randomIndex = Math.floor(Math.random() * arr.length);
+
+//   // get random item
+//   const item = arr[randomIndex];
+
+//   return item;
+// }
+
+// const array = ["H", "T"];
+
+// const result = getRandomItem(array);
+
+// document.getElementById("demo").innerHTML = result;
+// console.log(result);
